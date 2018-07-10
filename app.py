02f07f0450563876f2c556c137e6339f8de1a48c
James Regis <james.regis@bara.ca>
2018-07-10 13:48:28 -0400

    Change in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized - 1'
+    return 'Flask Dockerized - 1\n Flask Dockerized - 2'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
